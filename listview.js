ea68e16e27d710f3ae0e31e1b363b211896d4951
Jonathan Rehm <jkrehm@gmail.com>
2013-12-10 00:12:42 -0700

    More functionality and styling

--- listview.js
+++ listview.js
@@ -1,20 +1,55 @@
-define(['jquery', 'lodash', 'hammer', 'hammer-jquery'], function($, _, Hammer) {
+define(['jquery', 'hammer', 'hammer-jquery'], function($, Hammer) {
+
+    'use strict';
+
+    // Array forEach polyfill
+    if (!Array.prototype.forEach) {
+        Array.prototype.forEach = function (fn, scope) {
+            var i, len;
+            for (i = 0, len = this.length; i < len; ++i) {
+                if (i in this) {
+                    fn.call(scope, this[i], i, this);
+                }
+            }
+        };
+    }
+
+    function PercentToPx(breaks, elWidth) {
+
+        breaks.forEach(function(el, index) {
+    
+            if (!$.isArray(el.distance)) {
+                el.distance = [el.distance];
+            }
+
+            el.distance.forEach(function(startEnd, idx) {
+
+                if (typeof startEnd === 'string' && 
+                    startEnd.indexOf('%')
+                ) {
+                    breaks[index].distance[idx] = elWidth * startEnd.replace('%', '') / 100;
+                }
+            });
+        });
+
+        return breaks;
+    }
 
     function ListView(options) {
 
-        var listItem = this;
-
-        options = _.assign({
+        options = $.extend({
             selector : '',
             left     : [{
                 action   : function() {},
                 class    : '',
                 distance : [10],
+                html     : '',
             }],
             right    : [{
                 action   : function() {},
                 class    : '',
                 distance : [10],
+                html     : '',
             }],
             current  : {
                 action   : function() {},
@@ -25,40 +60,50 @@ define(['jquery', 'lodash', 'hammer', 'hammer-jquery'], function($, _, Hammer) {
         this.selector  = options.selector;
         this.left      = options.left;
         this.right     = options.right;
-        this.current   = options.current;
 
-        this.dragged   = false;
-        this.direction = '';
-        this.subitem   = '';
+        var dragged   = false;
+        var breaks, subitem, direction;
 
-        $(this.selector).hammer().on('drag', function(e) {
+        $(this.selector).hammer({
+            drag_min_distance : 2,
+        })
 
+        .on('dragstart', $.proxy(function(e) {
+        
+            direction = e.gesture.direction;
+
+            subitem = $('<div/>', {
+                'data-role' : 'subitem',
+            }).css({
+                height   : e.target.offsetHeight,
+                left     : e.target.offsetLeft,
+                position : 'absolute',
+                top      : e.target.offsetTop,
+                width    : e.target.offsetWidth,
+                'z-index' : e.target.style.zIndex - 1,
+            }).appendTo( $(e.target).parent() );
+
+            breaks = PercentToPx(this[direction], e.target.offsetWidth);
+        }, this))
+
+        .on('drag', $.proxy(function(e) {
+
+            e.gesture.preventDefault();
+
+            // Move the target
+            $(e.target).css({
+                transform : 'translate3d(' + e.gesture.deltaX + 'px, 0, 0)'
+            });
+
+            // Ignore drags that are not to the left or right
             if ([Hammer.DIRECTION_LEFT, Hammer.DIRECTION_RIGHT].indexOf(e.gesture.direction) === -1) {
                 return false;
             }
 
-            var distance = Math.abs(e.gesture.distance);
-
-            listItem.direction = e.gesture.direction;
-
-            // Make sub item
-            if (!listItem.dragged) {
-
-                listItem.subitem = $('<div/>', {
-                    'data-role' : 'subitem',
-                    class : listItem[listItem.direction][0].class,
-                });
-
-                listItem.subitem.css({
-                    height   : e.target.offsetHeight,
-                    left     : e.target.offsetLeft,
-                    position : 'absolute',
-                    top      : e.target.offsetTop,
-                    width    : e.target.offsetWidth,
-                    'z-index' : e.target.style.zIndex - 1,
-                });
-
-                $(e.target).parent().append(listItem.subitem);
+            // User changed direction, so recalculate the breaks
+            if (direction !== e.gesture.direction) {
+                direction = e.gesture.direction;
+                breaks = PercentToPx(this[direction], e.target.offsetWidth);
             }
 
             // Move the target
@@ -66,30 +111,47 @@ define(['jquery', 'lodash', 'hammer', 'hammer-jquery'], function($, _, Hammer) {
                 transform : 'translate3d(' + e.gesture.deltaX + 'px, 0, 0)'
             });
 
+            var distance = Math.abs(e.gesture.distance);
+
             // Determine which actions should be performed
-            listItem[listItem.direction].forEach(function(el, index) {
+            breaks.forEach(function(el, index) {
 
                 if (distance >= el.distance[0] && (typeof el.distance[1] == 'undefined' || distance < el.distance[1])) {
 
-                    listItem.current = el;
-                    listItem.subitem.removeClass().addClass(el.class);
+                    subitem.html(el.html);
+                    subitem.removeClass().addClass(el.class);
+                }
+
+            }, this);
+        }, this))
+
+        // Reset elements on release
+        .on('release', $.proxy(function(e) {
+
+            var current;
+
+            $(e.target).css({
+                transform : 'translate3d(0, 0, 0)'
+            });
+            $('[data-role="subitem"]').remove();
+
+            var distance = Math.abs(e.gesture.distance);
+
+            // Determine which actions should be performed
+            breaks.forEach(function(el, index) {
+
+                if (distance >= el.distance[0] + 20 && (typeof el.distance[1] == 'undefined' || distance < el.distance[1])) {
+                    current = index;
                 }
 
             }, this);
 
-            listItem.dragged = true;
-        });
+            if (typeof current != 'undefined') {
+                breaks[current].action(e);
+            }
 
-        $(this.selector).hammer().on('release', function(e) {
-
-            $(e.target).css({transform : 'translate3d(0, 0, 0)'});
-            $('[data-role="subitem"]').remove();
-
-            listItem.current.action({});
-
-            listItem.dragged = false;
-        });
-    };
+        }, this));
+    }
 
     return ListView;
 });
